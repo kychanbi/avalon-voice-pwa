a7bf34e9b11a9247668e4b71d5a89aac5b190b65
Trav <>
2023-07-12 00:11:02 +0800

    feat: Add translation command and configs

--- src/views/SettingTab.tsx
+++ src/views/SettingTab.tsx
@@ -4,16 +4,28 @@ import {
   Select,
   MenuItem,
   makeStyles,
-  FormControl, FormControlLabel, Checkbox, FormGroup, Slider, Typography, Fab,
-  Button, ButtonGroup, Tabs, Tab,
+  FormControl,
+  FormControlLabel,
+  Checkbox,
+  FormGroup,
+  Slider,
+  Typography,
+  Fab,
+  Button,
+  ButtonGroup,
+  Tabs,
+  Tab,
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import {
   AllSetting,
-  defaultAllSetting, GameMode, SettingContext,
+  defaultAllSetting,
+  GameMode,
+  SettingContext,
 } from '../state/playerSetting';
 import { Constants } from '../Constant';
 import { TabPanel } from './components/TabPanel';
+import { Trans } from '@lingui/macro';
 
 const useStyle = makeStyles((theme) => ({
   settingTab: {
@@ -46,35 +58,38 @@ const labels = Constants.label;
 
 const SettingTab = ({ closeDialog }: { closeDialog: EventHandler<any> }) => {
   const classes = useStyle();
-  const { allSetting, editSetting, editAllCharacterSettings } = useContext(SettingContext);
+  const { allSetting, editSetting, editAllCharacterSettings } =
+    useContext(SettingContext);
   const { avalonCharacterSetting, questCharacterSetting } = allSetting;
   const [formState, setFormState] = useState<AllSetting>(
-    allSetting ?? defaultAllSetting,
+    allSetting ?? defaultAllSetting
   );
   const handleSwitchTab = (event: React.ChangeEvent<{}>, value: GameMode) => {
     setFormState({ ...formState, gameMode: value });
     editSetting('gameMode', value);
   };
   const handleChange = <K extends keyof AllSetting>(
-    event: React.ChangeEvent<{ name?: string | undefined, value: unknown, checked?: boolean }>,
+    event: React.ChangeEvent<{
+      name?: string | undefined;
+      value: unknown;
+      checked?: boolean;
+    }>
   ) => {
     const name = event.target.name as K;
     const value = event.target.value as AllSetting[K];
     const checked = event.target.checked as boolean;
     const newVal = checked === undefined ? value : checked;
 
-
-      setFormState({ ...formState, [name]: newVal });
-      editSetting(name, newVal);
+    setFormState({ ...formState, [name]: newVal });
+    editSetting(name, newVal);
   };
-  const handleSliderChange = (name: string) => (
-    event: React.ChangeEvent<{}>,
-    value: number | number[],
-  ) => {
-    if (Array.isArray(value)) return;
-    setFormState({ ...formState, [name]: value });
-    editSetting(name, value);
-  };
+  const handleSliderChange =
+    (name: string) =>
+    (event: React.ChangeEvent<{}>, value: number | number[]) => {
+      if (Array.isArray(value)) return;
+      setFormState({ ...formState, [name]: value });
+      editSetting(name, value);
+    };
 
   const handlePresetClick = (id: string) => {
     const setting = Constants.presets[id];
@@ -87,111 +102,117 @@ const SettingTab = ({ closeDialog }: { closeDialog: EventHandler<any> }) => {
       <Fab className={classes.closeBtn} onClick={closeDialog}>
         <CloseIcon />
       </Fab>
-      <Tabs value={formState.gameMode} onChange={handleSwitchTab} aria-label='tab'>
-        <Tab label='Avalon' value={GameMode.Avalon} />
-        <Tab label='Quest' value={GameMode.Quest} />
+      <Tabs
+        value={formState.gameMode}
+        onChange={handleSwitchTab}
+        aria-label="tab"
+      >
+        <Tab label="Avalon" value={GameMode.Avalon} />
+        <Tab label="Quest" value={GameMode.Quest} />
       </Tabs>
       <TabPanel value={GameMode.Avalon} currentTab={formState.gameMode}>
-        <ButtonGroup className={classes.presetBtn} color='default'>
+        <ButtonGroup className={classes.presetBtn} color="default">
           {Object.keys(Constants.presets).map((key) => (
-            <Button key={key} id={key} onClick={(() => handlePresetClick(key))}>
+            <Button key={key} id={key} onClick={() => handlePresetClick(key)}>
               {' '}
               {Constants.presets[key].desc}
             </Button>
           ))}
         </ButtonGroup>
         <FormControl className={classes.selectFormControl}>
-          <InputLabel>總人數</InputLabel>
+          <InputLabel>
+            <Trans>總人數</Trans>
+          </InputLabel>
           <Select
-            labelId='select-total-number-of-player-label'
-            id='select-total-number-of-player'
+            labelId="select-total-number-of-player-label"
+            id="select-total-number-of-player"
             value={avalonCharacterSetting.totalNumberOfPlayer}
             onChange={handleChange}
-            name='avalonCharacterSetting.totalNumberOfPlayer'
+            name="avalonCharacterSetting.totalNumberOfPlayer"
           >
-            {Object.keys(
-              Constants.totalNumberOfPlayers,
-            ).map(
-              (n) => <MenuItem value={n} key={n}>{n}</MenuItem>,
-            )}
+            {Object.keys(Constants.totalNumberOfPlayers).map((n) => (
+              <MenuItem value={n} key={n}>
+                {n}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
         <FormGroup>
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={avalonCharacterSetting.isPercivalPresent}
                 onChange={handleChange}
-                name='avalonCharacterSetting.isPercivalPresent'
-                color='primary'
+                name="avalonCharacterSetting.isPercivalPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isPercivalPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={avalonCharacterSetting.isMordredPresent}
                 onChange={handleChange}
-                name='avalonCharacterSetting.isMordredPresent'
-                color='primary'
+                name="avalonCharacterSetting.isMordredPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isMordredPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={avalonCharacterSetting.isMorganaPresent}
                 onChange={handleChange}
-                name='avalonCharacterSetting.isMorganaPresent'
-                color='primary'
+                name="avalonCharacterSetting.isMorganaPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isMorganaPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={avalonCharacterSetting.isOberonPresent}
                 onChange={handleChange}
-                name='avalonCharacterSetting.isOberonPresent'
-                color='primary'
+                name="avalonCharacterSetting.isOberonPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isOberonPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={avalonCharacterSetting.isLancelotPresent}
                 onChange={handleChange}
-                name='avalonCharacterSetting.isLancelotPresent'
-                color='primary'
+                name="avalonCharacterSetting.isLancelotPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isLancelotPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={avalonCharacterSetting.useLancelotAlternativeRules}
                 onChange={handleChange}
-                name='avalonCharacterSetting.useLancelotAlternativeRules'
-                color='primary'
+                name="avalonCharacterSetting.useLancelotAlternativeRules"
+                color="primary"
               />
-            )}
+            }
             label={labels.useLancelotAlternativeRules}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={formState.isNewbieMode}
                 onChange={handleChange}
-                name='isNewbieMode'
-                color='primary'
+                name="isNewbieMode"
+                color="primary"
               />
-            )}
+            }
             label={labels.isNewbieMode}
           />
         </FormGroup>
@@ -199,107 +220,105 @@ const SettingTab = ({ closeDialog }: { closeDialog: EventHandler<any> }) => {
       <TabPanel value={GameMode.Quest} currentTab={formState.gameMode}>
         <FormGroup>
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isClericPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isClericPresent'
-                color='primary'
+                name="questCharacterSetting.isClericPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isClericPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isArthurPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isArthurPresent'
-                color='primary'
+                name="questCharacterSetting.isArthurPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isArthurPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isBlindHunterPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isBlindHunterPresent'
-                color='primary'
+                name="questCharacterSetting.isBlindHunterPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isBlindHunterPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isMorganLeFayPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isMorganLeFayPresent'
-                color='primary'
+                name="questCharacterSetting.isMorganLeFayPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isMorganLeFayPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isMutineerPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isMutineerPresent'
-                color='primary'
+                name="questCharacterSetting.isMutineerPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isMutineerPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isChangelingPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isChangelingPresent'
-                color='primary'
+                name="questCharacterSetting.isChangelingPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isChangelingPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isScionPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isScionPresent'
-                color='primary'
+                name="questCharacterSetting.isScionPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isScionPresent}
           />
           <FormControlLabel
-            control={(
+            control={
               <Checkbox
                 checked={questCharacterSetting.isLancelotPresent}
                 onChange={handleChange}
-                name='questCharacterSetting.isLancelotPresent'
-                color='primary'
+                name="questCharacterSetting.isLancelotPresent"
+                color="primary"
               />
-            )}
+            }
             label={labels.isLancelotPresent}
           />
         </FormGroup>
       </TabPanel>
       <div className={classes.sliderWrap}>
         <Typography gutterBottom>
-          speaking speed :
-          {' '}
-          {formState.speakingRate}
+          speaking speed : {formState.speakingRate}
         </Typography>
         <Slider
-          name='speakingRate'
+          name="speakingRate"
           defaultValue={formState.speakingRate}
           value={formState.speakingRate}
           onChange={handleSliderChange('speakingRate')}
-          valueLabelDisplay='auto'
+          valueLabelDisplay="auto"
           min={0.5}
           max={1.5}
           step={0.05}
@@ -307,16 +326,14 @@ const SettingTab = ({ closeDialog }: { closeDialog: EventHandler<any> }) => {
       </div>
       <div className={classes.sliderWrap}>
         <Typography gutterBottom>
-          counting speed :
-          {' '}
-          {formState.countingRate}
+          counting speed : {formState.countingRate}
         </Typography>
         <Slider
-          name='countingRate'
+          name="countingRate"
           defaultValue={formState.countingRate}
           value={formState.countingRate}
           onChange={handleSliderChange('countingRate')}
-          valueLabelDisplay='auto'
+          valueLabelDisplay="auto"
           min={0.6}
           max={1.6}
           step={0.06}
